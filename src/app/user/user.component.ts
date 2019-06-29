928d94dbc4afd5101d1af1b776089c9b19d2d100
QuyenLuong <ducquyenluong97@gmail.com>
2019-06-29 18:17:11 +0700

    Fineee

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,22 +1,78 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
-import { Observable } from 'rxjs';
-
+import { Observable, BehaviorSubject } from "rxjs";
+import { map, tap, mergeMap, scan, throttleTime, reduce } from "rxjs/operators";
+import { CdkVirtualScrollViewport } from "@angular/cdk/scrolling";
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
-  users: Observable<any>;
-  // displayedColumns = ['id', 'name', 'gender', 'address', 'weight'];
-  // dataSource;
+  lastDocView$ = new BehaviorSubject<any>(null); // Get the last ID value of the current sub-data.
+  dataShowOnScreen$: Observable<any[]>; // Show data
+  stop = new BehaviorSubject<boolean>(false); // Stop when all data gone.
+
+  @ViewChild(CdkVirtualScrollViewport) viewport: CdkVirtualScrollViewport;
+
   constructor(private fb: AngularFirestore) {}
 
   ngOnInit(): void {
-    this.users = this.fb
-      .collection("users")
-      .valueChanges();
+    const dataScroll = this.lastDocView$.pipe(
+      mergeMap(val => this.getSubData(val)),
+      scan((acc, cur) => {
+        console.log(acc);
+        console.log(cur);
+        return { ...acc, ...cur };
+      }, {})
+    );
+
+    this.dataShowOnScreen$ = dataScroll.pipe(
+      map(v => {
+        console.log(Object.values(v));
+        return Object.values(v);
+      })
+    );
+  }
+
+  getSubData(lastDocView) {
+    return this.fb
+      .collection("users", ref =>
+        ref
+          .orderBy("id", "asc")
+          .startAfter(lastDocView)
+          .limit(20)
+      )
+      .snapshotChanges()
+      .pipe(
+        tap(doc => {
+          console.log(doc);
+          if (doc.length === 0) {
+            console.log('check');
+            this.stop.next(true);
+          }
+        }),
+        map(arr => {
+          return arr.reduce((acc, cur) => {
+            const id = cur.payload.doc.id;
+            const data = cur.payload.doc.data();
+            return { ...acc, [id]: data };
+          }, {});
+        })
+      );
+  }
+
+  nextData(e, lastDocView) {
+    if (this.stop.value) {
+      return;
+    }
+    const dataLen = this.viewport.getDataLength();
+    const range = this.viewport.getRenderedRange().end;
+    console.log(dataLen, " ", range);
+    if (dataLen === range) {
+      this.lastDocView$.next(lastDocView);
+    }
+    return null;
   }
 
   trackByIdx(i) {
